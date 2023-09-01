2d16d87a2091aee701e7445ff659b27e165f81e3
JayBassCodes <jacob.m.bassett@gmail.com>
2023-09-01 15:21:56 -0400

    finished app including README and tests

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Logo-Generator
+# SVG Logo-Generator
+## Use this program to make a simple logo that saves to a .svg file
+
+
+This program will generate a simple logo consisting of a triangle, square, or circle of any color of your choosing with a 3 character text overlayed and also in any color of your choosing.  The program runs from your command prompt using Inquirer and FS to generate your logo and can be tested using Jest.  
+
+
+How to use app:
+
+    - Install Node.js if you have not already
+
+    - Clone this repo to your local machine
+
+    - Open the command prompt to the app directory
+
+    - Run npm I
+
+    - Run node index.js
+
+    - Follow the prompts and your logo will be made shortly
+
+
+
+Example (after installing Node.js and running npm i):
+
+![Alt text](<Mod10 Command Prompt Gif.gif>)
+
+
+
+
+![Alt text](<Mod10 Shape Gif.gif>)
+
+
+
+
+![Alt text](<Mod10 Test Gif.gif>)
--- index.js
+++ index.js
@@ -1,113 +1,95 @@
-const inquirer = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
 const { Circle, Square, Triangle } = require('./lib/shapes');
 
-// start of inquirer
-inquirer.prompt([
-    // inquiry for logo text
-    {
-        name: 'Logo Text',
-        message: 'Please input the text you would like on your logo.',
-        type: 'input',
-        filter: function(input) {
-            return input.trim().toUpperCase();
-        },
-        validate: function(input) {
-            if (input.length === 0 || input.length > 3) {
-                return 'Logo text must be at least one character and no more than three characters.';
-            } else {
-                return true;
-            }
-        },
-    },
-
-    // inquiry for font color
-    {
-        name: 'Logo Font Color',
-        message: 'What color would you like the text?',
-        type: 'input',
-        filter: function(input) {
-            return input.trim().toLowerCase();
-        },
-        validate: validateColor,
-    },
-
-    // inquiry for what shape the logo will be
-    {
-        name: 'Logo Shape',
-        message: 'Please select a shape for your logo',
-        type: 'list',
-        choices: [
-            {
-                name: 'Circle',
-                value: new Circle(),
-            },
-            {
-                name: 'Square',
-                value: new Square(),
-            },
-            {
-                name: 'Triangle',
-                value: new Triangle(),
-            }
-        ]
-    },
-
-    {
-        name: 'logoBgColor',
-        message: 'Please enter a color name or hexdecimal code for your logo background',
-        type: 'input',
-        filter: function(input) {
-            return input.trim().toLowerCase();
-        },
-        validate: validateColor,
-    },
-]).then(answer => {
-    console.log(answer);
-    
-    svgMaker(answer);
-})
-
-// dunction to produce .svg
-function svgMaker( { logoText, logoFontColor, logoShape, logoBgColor } ) {
-    logoShape.setLogoColor(logoBgColor);
-    const svg = `
-        <svg version="1.1"
-            width="300" height="200"
-            xmlns="http://www.w3.org/2000/svg">
-            <rect width="100%" height="100%" fill="transparent" />
-
-            ${logoShape.renderLogoColor()}
-
-            ${logoShape.renderLogoText(logoText, logoFontColor)}
-        </svg>`;
-
-    writeToFile('logo.svg', svg);
-}
-
-
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (error) => {
-        if (error) {
-            console.error(error);
-            return;
-        } else {
-            console.log('Success!');
-        }
-    })
-}
-
-function validateColor(nameOrHex) {
-
-    // checks if input is color name or hexdec
-    const hexColorRegex = /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/;
-
-    if (hexColorRegex.test(nameOrHex)) {
-      return true;
-    } else if (isCssColorName(nameOrHex)) {
-      return true;
-    } else {
-      console.log(` is not a valid SVG color name or hexadecimal code.`);
-      return false;
+class Svg {
+    constructor() {
+      this.textElement = "";
+      this.shapeElement = "";
     }
-}
+    render(){
+      return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color) {
+      this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape) {
+      this.shapeElement = shape.render()
+    } 
+  }
+  
+  const params = [
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to 3 letters for the center of your logo:",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter a color by name or hexadecimal code for the text:",
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Enter a color by name or hexadecimal code for the shape:"
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Which shape would you like to use for your logo?",
+      choices: ["Circle", "Square", "Triangle"]
+    },
+  ]
+  
+  function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('Success, logo written to .svg file')
+    })
+  }
+  
+  async function init() {
+    var svgString = "";
+    var svgFile = "logo.svg";
+  
+      const answers = await inquirer.prompt(params);
+  
+    var userText = "";
+    if (answers.text.length > 0 && answers.text.length < 4) {
+      userText = answers.text;
+    } else {
+      console.log("Invalid input, Please enter up to 3 Characters");
+        return;
+    }
+  
+    userTextColor = answers.textColor;
+    userShape = answers.shape;
+    userShapeColor = answers.shapeColor;
+  
+    let newShape;
+    if(userShape === "Circle") {
+      newShape = new Circle();
+  
+    } else if (userShape === "Square") {
+      newShape = new Square();
+  
+    } else if (userShape === "Triangle") {
+      newShape = new Triangle();
+  
+    } else {
+      console.log("Invalid Shape!")
+    }
+    newShape.setColor(userShapeColor);
+  
+    var svg = new Svg();
+    svg.setTextElement(userText, userTextColor);
+    svg.setShapeElement(newShape);
+    svgString = svg.render();
+  
+    writeToFile(svgFile, svgString);
+  }
+  
+  init()
